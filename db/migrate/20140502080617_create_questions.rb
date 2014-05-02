bb10a3f1e0d466b5d58e54a3249c3c63c4e3f5af
Ilya Tcykunov <ilya.tcykunov@gmail.com>
2014-05-02 13:19:25 +0400

    Ссылка на пользователя в questions сделана обязательной

--- db/migrate/20140502080617_create_questions.rb
+++ db/migrate/20140502080617_create_questions.rb
@@ -3,7 +3,7 @@ class CreateQuestions < ActiveRecord::Migration
     create_table :questions do |t|
       t.text :title, null: false
       t.text :body, null: false
-      t.references :user, index: true
+      t.references :user, index: true, null: false
 
       t.timestamps
     end
