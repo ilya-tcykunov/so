26ae4a09100173c188971bd01da73565cf3fdda1
Ilya Tcykunov <ilya.tcykunov@gmail.com>
2014-05-10 13:32:34 +0400

    Создана модель answer

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -4,4 +4,6 @@ describe Question do
   it {should validate_presence_of(:title)}
   it {should validate_presence_of(:body)}
   it {should validate_presence_of(:user)}
+  it {should belong_to(:user)}
+  it {should have_many(:answers)}
 end
